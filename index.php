866c2bf7bcbceb932cdd8474cce78d4121010529
Gee Actub <boygee2025@gmail.com>
2020-09-09 19:28:43 +0800

    with controls

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
     <title>Yes</title>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDcbYr1unjyMn3gcwaDyrVfdn9bW2ELBjk&callback=initMap&libraries=&v=weekly"
-      defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDcbYr1unjyMn3gcwaDyrVfdn9bW2ELBjk&callback=initMap&libraries=&v=weekly" defer
     ></script>
     <link rel="stylesheet" type="text/css" href="css/style.css" />
     <script src="js/app.js"></script>
@@ -15,9 +14,17 @@
         <input onclick="clearMarkers();" type="button" value="Hide Markers" />
         <input onclick="showMarkers();" type="button" value="Show All Markers" />
         <input onclick="deleteMarkers();" type="button" value="Delete Markers" />
-      </div>
+    </div>
     <div class="map-container">
       <div id="map"></div>
+    </div>
+    <br>
+    <div class="navigation-controller" style="text-align:center;">
+        <input onclick="moveUp();" type="button" value="Walk up ˄" />
+        <input onclick="moveDown();" type="button" value="Walk down ˅" />
+        <input onclick="moveLeft();" type="button" value="Walk Left ˂" />
+        <input onclick="moveRight();" type="button" value="Walk Right ˃" />
+        <input onclick="findMe();" type="button" value="Find me" />
     </div>
     <div style="text-align:center;">
     <br>
--- js/app.js
+++ js/app.js
@@ -6,131 +6,234 @@ var icons = {
     icon: iconBase + 'tenor2.gif'
   },
   library: {
-    icon: iconBase + 'library_maps.png'
+    icon: iconBase + 'house.gif'
   },
   info: {
     icon: iconBase + 'info-i_maps.png'
   }
 };
+let current_location = { lat: 35.710, lng: 139.753 };
 function initMap() {
-  const haightAshbury = { lat: 35.714352, lng: 139.7524633 };
+  
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 17,
     disableDefaultUI: true,
     minZoom: 17,
     maxZoom: 17,
     zoomControl: false,
-    center: haightAshbury,
+    center: current_location,
     mapTypeId: "terrain",
     styles: [
         {
-            "featureType": "all",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "saturation": "32"
-                },
-                {
-                    "lightness": "-3"
-                },
-                {
-                    "visibility": "on"
-                },
-                {
-                    "weight": "1.18"
-                }
-            ]
-        },
-        {
-            "featureType": "administrative",
             "elementType": "labels",
             "stylers": [
                 {
                     "visibility": "off"
+                },
+                {
+                    "color": "#f49f53"
                 }
             ]
         },
         {
             "featureType": "landscape",
-            "elementType": "labels",
             "stylers": [
                 {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "landscape.man_made",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "saturation": "-70"
+                    "color": "#f9ddc5"
                 },
                 {
-                    "lightness": "14"
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "labels",
-            "stylers": [
-                {
-                    "visibility": "off"
+                    "lightness": -7
                 }
             ]
         },
         {
             "featureType": "road",
-            "elementType": "labels",
+            "stylers": [
+                {
+                    "color": "#813033"
+                },
+                {
+                    "lightness": 43
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business",
+            "stylers": [
+                {
+                    "color": "#645c20"
+                },
+                {
+                    "lightness": 38
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "stylers": [
+                {
+                    "color": "#1994bf"
+                },
+                {
+                    "saturation": -69
+                },
+                {
+                    "gamma": 0.99
+                },
+                {
+                    "lightness": 43
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#f19f53"
+                },
+                {
+                    "weight": 1.3
+                },
+                {
+                    "visibility": "on"
+                },
+                {
+                    "lightness": 16
+                }
+            ]
+        },
+        {
+            "featureType": "poi.business"
+        },
+        {
+            "featureType": "poi.park",
+            "stylers": [
+                {
+                    "color": "#645c20"
+                },
+                {
+                    "lightness": 39
+                }
+            ]
+        },
+        {
+            "featureType": "poi.school",
+            "stylers": [
+                {
+                    "color": "#a95521"
+                },
+                {
+                    "lightness": 35
+                }
+            ]
+        },
+        {},
+        {
+            "featureType": "poi.medical",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#813033"
+                },
+                {
+                    "lightness": 38
+                },
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {
+            "elementType": "labels"
+        },
+        {
+            "featureType": "poi.sports_complex",
+            "stylers": [
+                {
+                    "color": "#9e5916"
+                },
+                {
+                    "lightness": 32
+                }
+            ]
+        },
+        {},
+        {
+            "featureType": "poi.government",
+            "stylers": [
+                {
+                    "color": "#9e5916"
+                },
+                {
+                    "lightness": 46
+                }
+            ]
+        },
+        {
+            "featureType": "transit.station",
             "stylers": [
                 {
                     "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit.line",
+            "stylers": [
+                {
+                    "color": "#813033"
+                },
+                {
+                    "lightness": 22
                 }
             ]
         },
         {
             "featureType": "transit",
-            "elementType": "labels",
             "stylers": [
                 {
-                    "visibility": "off"
+                    "lightness": 38
                 }
             ]
         },
         {
-            "featureType": "water",
-            "elementType": "all",
+            "featureType": "road.local",
+            "elementType": "geometry.stroke",
             "stylers": [
                 {
-                    "saturation": "100"
+                    "color": "#f19f53"
                 },
                 {
-                    "lightness": "-14"
+                    "lightness": -10
                 }
             ]
         },
-        {
-            "featureType": "water",
-            "elementType": "labels",
-            "stylers": [
-                {
-                    "visibility": "off"
-                },
-                {
-                    "lightness": "12"
-                }
-            ]
-        }
+        {},
+        {},
+        {}
     ]
   });
   // This event listener will call addMarker() when the map is clicked.
   map.addListener("click", event => {
-    deleteMarkers();
+    //deleteMarkers();
+    clearMarkers();
     addMarker(event.latLng);
   });
   // Adds a marker at the center of the map.
-  addMarker(haightAshbury);
+  addMarker(current_location);
+  //randomMarker(haightAshbury);
 }
 
 // Adds a marker to the map and push to the array.
@@ -144,6 +247,15 @@ function addMarker(location) {
   markers.push(marker);
 }
 
+function randomMarker(location) {
+    const marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      icon: icons['library'].icon
+    });
+    
+    markers.push(marker);
+  }
 // Sets the map on all markers in the array.
 function setMapOnAll(map) {
   for (let i = 0; i < markers.length; i++) {
@@ -165,4 +277,35 @@ function showMarkers() {
 function deleteMarkers() {
   clearMarkers();
   markers = [];
+}
+function moveUp(){
+    current_location = { lat: current_location.lat + 0.001, lng: current_location.lng };
+    console.log(current_location);
+    deleteMarkers();
+    addMarker(current_location);
+    //map.setCenter(current_location);
+}
+function moveDown(){
+    current_location = { lat: current_location.lat - 0.001, lng: current_location.lng };
+    console.log(current_location);
+    deleteMarkers();
+    addMarker(current_location);
+    //map.setCenter(current_location);
+}
+function moveLeft(){
+    current_location = { lat: current_location.lat, lng: current_location.lng - 0.001 };
+    console.log(current_location);
+    deleteMarkers();
+    addMarker(current_location);
+    //map.setCenter(current_location);
+}
+function moveRight(){
+    current_location = { lat: current_location.lat, lng: current_location.lng + 0.001 };
+    console.log(current_location);
+    deleteMarkers();
+    addMarker(current_location);
+    //map.setCenter(current_location);
+}
+function findMe(){
+    map.setCenter(current_location);
 }
